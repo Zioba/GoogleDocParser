12b329807c604635b3312d5c76d04f0453fc0258
Alexander Yurkov <ayurkov@elilink.com>
2018-12-04 17:33:38 +0300

    implemented automatic deletion of rights, refactoring

--- main.py
+++ main.py
@@ -13,49 +13,40 @@ def getCharNickname(s):
 
 def main():
     CREDENTIALS_FILE = 'wowparserassist-bfeb2fb346fa.json' # имя файла с закрытым ключом
+    docId = '1IKgfrzC2yIzMUABSQbJoU34xu1BWdHzraS7TbmoNulI'  # Спектр
     tools = SpreadsheetTools(CREDENTIALS_FILE, debugMode=True)
-    #docId = '17MvXorJo_stNi8129gOStuTrGKdfO5cf-3x82ddqXdQ' #тестовый док
-    #docId = '1eI61oFGr1BmPjvI3rlrwbxQvOq6CDNvA1z1P19zSLRQ' #это мой ID
-    docId = '1IKgfrzC2yIzMUABSQbJoU34xu1BWdHzraS7TbmoNulI' #Спектр
     tools.setSpreadsheetById(docId)
-    tools.setSheet('Чары 12')
 
     credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, ['https://www.googleapis.com/auth/spreadsheets',
                                                                                   'https://www.googleapis.com/auth/drive'])
     httpAuth = credentials.authorize(httplib2.Http())
     service = apiclient.discovery.build('sheets', 'v4', http = httpAuth)
-    request = service.spreadsheets().get(spreadsheetId= docId, ranges= [], includeGridData= False)
-    response = request.execute()
+
 #чары 12 лист
-    '''
+    tools.setSheet('Чары 12')
     request = service.spreadsheets().values().get(spreadsheetId=docId, range="Чары 12!A:A")
     response = request.execute()
+    tools.prepare_setValues("H1:H1", [[str(datetime.datetime.now().date())]])
+    tools.runPrepared()
     charNicknames = response.get('values')
     t = 0;
     for i in charNicknames:
         if (t != 0):
+            values = 'B' + str(t + 1) + ':C' + str(t + 1);
             try:
                 player = getCharNickname(i)
                 URL = 'https://eu.api.battle.net/wow/character/ревущий-фьорд/' + player + '?fields=items&locale=ru_RU&apikey=a54rf7cgky8w4nt6hwjt67bwkdmkkpbp'
-                r = requests.get(URL)
-                json = r.json()
-                items = json.get('items')
-                averangeItemLevelEquipped = items.get('averageItemLevelEquipped')
-                item2 = items.get('neck')
-                neckItemLevel = item2.get('itemLevel')
-                item3 = item2.get('azeriteItem')
-                artLevel = item3.get('azeriteLevel')
-                values = 'B' + str(t + 1) + ':C' + str(t + 1);
+                json = requests.get(URL).json()
+                averangeItemLevelEquipped = json.get('items').get('averageItemLevelEquipped')
+                neck = json.get('items').get('neck')
+                neckItemLevel = neck.get('itemLevel')
+                artLevel = neck.get('azeriteItem').get('azeriteLevel')
                 tools.prepare_setValues(values,
-                                        [[str(neckItemLevel) + '(' + str(artLevel) + ')', averangeItemLevelEquipped]])
-                tools.runPrepared()
+                                        [[averangeItemLevelEquipped, str(artLevel) + ' (' + str(neckItemLevel) + ')']])
             except:
-                values = 'B' + str(t + 1) + ':C' + str(t + 1);
                 tools.prepare_setValues(values,
                                         [['error', 'error']])
-                tools.runPrepared()
-                t += 1
-                continue
+            tools.runPrepared()
         t += 1
 #чары 20 лист мейны
     tools.setSheet('Чары 20')
@@ -65,63 +56,26 @@ def main():
     t = 0;
     for i in charNicknames:
         if (t != 0):
+            values = 'B' + str(t + 1) + ':C' + str(t + 1);
             try:
                 player = getCharNickname(i)
                 URL = 'https://eu.api.battle.net/wow/character/ревущий-фьорд/' + player + '?fields=items&locale=ru_RU&apikey=a54rf7cgky8w4nt6hwjt67bwkdmkkpbp'
-                r = requests.get(URL)
-                json = r.json()
-                items = json.get('items')
-                averangeItemLevelEquipped = items.get('averageItemLevelEquipped')
-                item2 = items.get('neck')
-                neckItemLevel = item2.get('itemLevel')
-                item3 = item2.get('azeriteItem')
-                artLevel = item3.get('azeriteLevel')
-                values = 'B' + str(t + 1) + ':C' + str(t + 1);
+                json = requests.get(URL).json()
+                averangeItemLevelEquipped = json.get('items').get('averageItemLevelEquipped')
+                neck = json.get('items').get('neck')
+                neckItemLevel = neck.get('itemLevel')
+                artLevel = neck.get('azeriteItem').get('azeriteLevel')
                 tools.prepare_setValues(values,
-                                        [[str(neckItemLevel) + '(' + str(artLevel) + ')', averangeItemLevelEquipped]])
-                tools.runPrepared()
+                                        [[averangeItemLevelEquipped,
+                                          str(artLevel) + ' (' + str(neckItemLevel) + ')']])
             except:
-                values = 'B' + str(t + 1) + ':C' + str(t + 1);
                 tools.prepare_setValues(values,
                                         [['error', 'error']])
-                tools.runPrepared()
-                t += 1
-                continue
+            tools.runPrepared()
         t += 1
+
 #чары 20 лист твинки
     request = service.spreadsheets().values().get(spreadsheetId=docId, range="Чары 20!D:D")
-    response = request.execute()
-    charNicknames = response.get('values')
-    t = 0;
-    for i in charNicknames:
-        if (t != 0):
-            try:
-                player = getCharNickname(i)
-                URL = 'https://eu.api.battle.net/wow/character/ревущий-фьорд/' + player + '?fields=items&locale=ru_RU&apikey=a54rf7cgky8w4nt6hwjt67bwkdmkkpbp'
-                r = requests.get(URL)
-                json = r.json()
-                items = json.get('items')
-                averangeItemLevelEquipped = items.get('averageItemLevelEquipped')
-                item2 = items.get('neck')
-                neckItemLevel = item2.get('itemLevel')
-                item3 = item2.get('azeriteItem')
-                artLevel = item3.get('azeriteLevel')
-                values = 'E' + str(t + 1) + ':F' + str(t + 1);
-                tools.prepare_setValues(values,
-                                        [[str(neckItemLevel) + '(' + str(artLevel) + ')', averangeItemLevelEquipped]])
-                tools.runPrepared()
-            except:
-                values = 'E' + str(t + 1) + ':F' + str(t + 1);
-                tools.prepare_setValues(values,
-                                        [['error', 'error']])
-                tools.runPrepared()
-                t += 1
-                continue
-        t += 1
-    '''
-    tools.setSheet('Доступы')
-    tools.setSheetNumbId('Доступы') #функция костыльная, там забит id листа вручную
-    request = service.spreadsheets().values().get(spreadsheetId=docId, range="Доступы!A:A")
     response = request.execute()
     tools.prepare_setValues("H1:H1", [[str(datetime.datetime.now().date())]])
     tools.runPrepared()
@@ -129,78 +83,121 @@ def main():
     t = 0;
     for i in charNicknames:
         if (t != 0):
+            values = 'E' + str(t + 1) + ':F' + str(t + 1);
+            try:
+                player = getCharNickname(i)
+                URL = 'https://eu.api.battle.net/wow/character/ревущий-фьорд/' + player + '?fields=items&locale=ru_RU&apikey=a54rf7cgky8w4nt6hwjt67bwkdmkkpbp'
+                json = requests.get(URL).json()
+                averangeItemLevelEquipped = json.get('items').get('averageItemLevelEquipped')
+                neck = json.get('items').get('neck')
+                neckItemLevel = neck.get('itemLevel')
+                artLevel = neck.get('azeriteItem').get('azeriteLevel')
+                tools.prepare_setValues(values,
+                                        [[averangeItemLevelEquipped, str(artLevel) + ' (' + str(neckItemLevel) + ')']])
+            except:
+                tools.prepare_setValues(values,
+                                        [['error', 'error']])
+            tools.runPrepared()
+        t += 1
+#доступы
+    tools.setSheet('Доступы')
+    tools.setSheetNumbId('Доступы') #функция костыльная, там забит id листа вручную
+    request = service.spreadsheets().values().get(spreadsheetId=docId, range="Доступы!A:A")
+    response = request.execute()
+    charNicknames = response.get('values')
+    request = service.spreadsheets().values().get(spreadsheetId=docId, range="Доступы!B:B")
+    response = request.execute()
+    charRoles = response.get('values')
+    request = service.spreadsheets().values().get(spreadsheetId=docId, range="Доступы!C:C")
+    response = request.execute()
+    charEmails = response.get('values')
+    request = service.spreadsheets().values().get(spreadsheetId=docId, range="Доступы!E:E")
+    response = request.execute()
+    charStatus = response.get('values')
+    tools.prepare_setValues("H1:H1", [[str(datetime.datetime.now().date())]])
+    tools.runPrepared()
+    t = 0;
+    for i in charNicknames:
+        if (t > 1):
+            try:
+                status = getCharNickname(charStatus[t])
+            except:
+                status = "1"
             try:
                 player = getCharNickname(i)
                 URL = 'https://eu.api.battle.net/wow/character/ревущий-фьорд/' + player + '?fields=guild&locale=ru_RU&apikey=a54rf7cgky8w4nt6hwjt67bwkdmkkpbp'
                 r = requests.get(URL)
                 json = r.json()
                 print(json)
-
-                guild = json.get('guild')
-                guildName = guild.get('name')
-                values = 'D' + str(t + 1) + ':E' + str(t + 1);
+                try:
+                    guild = json.get('guild')
+                    guildName = guild.get('name')
+                except:
+                    guildName = "no guild"
+                values = 'D' + str(t + 1) + ':F' + str(t + 1);
                 if (guildName == 'Спектр'):
-                    tools.prepare_setValues(values, [['OK' , guildName]])
-                    tools.runPrepared()
-                    tools.prepare_setCellsFormats(values,[[{'backgroundColor': {'red': 0, 'green': 1, 'blue': 0}},
-                                                           {'backgroundColor': {'red': 1, 'green': 1, 'blue': 1}}]])
-                    tools.runPrepared()
-                else:
-                    tools.prepare_setValues(values, [['чел не в гильдии', guildName]])
-                    tools.runPrepared()
-                    tools.prepare_setCellsFormats(values, [[{'backgroundColor': {'red': 1, 'green': 0, 'blue': 0}},
+                    try:
+                        driveService = apiclient.discovery.build('drive', 'v3', http=httpAuth)
+                        if (status != 'права выданы'):
+                            role = getCharNickname(charRoles[t])
+                            email = getCharNickname(charEmails[t])
+                            driveService.permissions().create(
+                                fileId=docId,
+                                body={'type': 'user', 'role': role, 'emailAddress': email},
+                            ).execute()
+                            tools.prepare_setValues(values, [['OK', 'права выданы', '']])
+                            tools.runPrepared()
+                            tools.prepare_setCellsFormats(values,
+                                                          [[{'backgroundColor': {'red': 0, 'green': 1, 'blue': 0}},
+                                                            {'backgroundColor': {'red': 1, 'green': 1, 'blue': 1}},
                                                             {'backgroundColor': {'red': 1, 'green': 1, 'blue': 1}}]])
-                    tools.runPrepared()
+                            tools.runPrepared()
+                    except:
+                        tools.prepare_setValues(values, [['FAIL', 'неправильная роль или почта', 'права не выданы']])
+                        tools.runPrepared()
+                        tools.prepare_setCellsFormats(values, [[{'backgroundColor': {'red': 1, 'green': 0, 'blue': 0}},
+                                                                {'backgroundColor': {'red': 1, 'green': 1, 'blue': 1}},
+                                                                {'backgroundColor': {'red': 1, 'green': 1, 'blue': 1}}]])
+                        tools.runPrepared()
+                        t += 1
+                        continue
+                else:
+                    if (status == 'права выданы'):
+                        email = getCharNickname(charEmails[t])
+                        driveService = apiclient.discovery.build('drive', 'v2', http=httpAuth)
+                        permissions = driveService.permissions().getIdForEmail(email=email).execute()
+                        print(permissions['id'])
+                        driveService.permissions().delete(
+                            fileId = docId,
+                            permissionId=permissions['id']
+                        ).execute()
+                        tools.prepare_setValues(values, [['FAIL', 'чел в другой гильдии', 'права удалены']])
+                        tools.runPrepared()
+                        tools.prepare_setCellsFormats(values, [[{'backgroundColor': {'red': 1, 'green': 0, 'blue': 0}},
+                                                                {'backgroundColor': {'red': 1, 'green': 0, 'blue': 0}},
+                                                                {'backgroundColor': {'red': 1, 'green': 1,
+                                                                                     'blue': 1}}]])
+                        tools.runPrepared()
             except:
-                values = 'D' + str(t + 1) + ':E' + str(t + 1);
+                if (status == 'права выданы'):
+                    email = getCharNickname(charEmails[t])
+                    driveService = apiclient.discovery.build('drive', 'v2', http=httpAuth)
+                    permissions = driveService.permissions().getIdForEmail(email=email).execute()
+                    print(permissions['id'])
+                    driveService.permissions().delete(
+                        fileId=docId,
+                        permissionId=permissions['id']
+                    ).execute()
+                values = 'D' + str(t + 1) + ':F' + str(t + 1);
                 tools.prepare_setValues(values,
-                                        [['персонажа не существует', 'error']])
+                                        [['FAIL', 'персонажа не существует', 'права удалены']])
                 tools.runPrepared()
-                t += 1
-                continue
+                tools.prepare_setCellsFormats(values, [[{'backgroundColor': {'red': 1, 'green': 0, 'blue': 0}},
+                                                        {'backgroundColor': {'red': 1, 'green': 1, 'blue': 1}},
+                                                        {'backgroundColor': {'red': 1, 'green': 1, 'blue': 1}}]])
+                tools.runPrepared()
         t += 1
 
 
 if __name__ == "__main__":
     main()
-'''
-CREDENTIALS_FILE = 'wowparserassist-bfeb2fb346fa.json'  # имя файла с закрытым ключом
-credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, ['https://www.googleapis.com/auth/spreadsheets',
-                                                                                  'https://www.googleapis.com/auth/drive'])
-httpAuth = credentials.authorize(httplib2.Http())
-service = apiclient.discovery.build('sheets', 'v4', http = httpAuth)
-#1eI61oFGr1BmPjvI3rlrwbxQvOq6CDNvA1z1P19zSLRQ #это мой ID
-docId = '17MvXorJo_stNi8129gOStuTrGKdfO5cf-3x82ddqXdQ' #тестовый док
-#spreadsheet = service.spreadsheets().create(body = {
-#    'properties': {'title': 'Первый тестовый документ', 'locale': 'ru_RU'},
-#    'sheets': [{'properties': {'sheetType': 'GRID',
-#                               'sheetId': 0,
-#                               'title': 'Сие есть название листа',
-#                               'gridProperties': {'rowCount': 8, 'columnCount': 5}}}]
-#}).execute()
-request = service.spreadsheets().get(spreadsheetId= docId, ranges= [], includeGridData= False)
-response = request.execute()
-print(response)
-#driveService = apiclient.discovery.build('drive', 'v3', http = httpAuth)
-#shareRes = driveService.permissions().create(
-#    fileId = docId,
-#    body = {'type': 'user', 'role': 'writer', 'emailAddress': 'zioba1404@gmail.com'},
-#).execute()
-request = service.spreadsheets().values().get(spreadsheetId=docId, range="Сие есть название листа!B2:C2", valueRenderOption='', dateTimeRenderOption='')
-response = request.execute()
-pprint(response)
-
-results = service.spreadsheets().values().batchUpdate(spreadsheetId = docId, body = {
-    "valueInputOption": "USER_ENTERED",
-    "data": [
-        {"range": "Сие есть название листа!B2:C2",
-         "majorDimension": "ROWS",     # сначала заполнять ряды, затем столбцы (т.е. самые внутренние списки в values - это ряды)
-         "values": [["Azerite Level", "Item Level"]]}
-    ]
-}).execute()
-s=requests.get('https://www.wowprogress.com/character/eu/ревущий-фьорд/Зёба')
-    b=bs4.BeautifulSoup(s.text, "html.parser")
-    p3=b.select('.char_rating_area .gearscore')
-    pogoda1=p3[3].getText()
-    print('Azerite Level:' + pogoda1)
-'''
